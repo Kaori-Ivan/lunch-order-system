d0edaba63dc6b3e90102ef66ec56d6733a1e783d
Kaori-Ivan <soppsx@yahoo.com.tw>
2026-06-29 14:53:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const APP_CONFIG = {
   MODE: "TEST",
   DEADLINE_HOUR: 10,
   DEADLINE_MINUTE: 0,
-  API_TIMEOUT_MS: 10000,
+  API_TIMEOUT_MS: 30000,
   API_URL: "https://script.google.com/macros/s/AKfycbycUcCxk0nPzR8iS5_J9jwttu1Dvftwg9kJFJtnd_0abTksYgvCMabvl0gvis5ov78/exec"
 };
