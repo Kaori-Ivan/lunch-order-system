5d841887a26226f6f2e1da16d9c0e41123a8f766
Kaori-Ivan <soppsx@yahoo.com.tw>
2026-07-13 13:48:38 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const APP_CONFIG = {
+  MODE: "TEST",
+  DEADLINE_HOUR: 10,
+  DEADLINE_MINUTE: 0,
+  USE_MOCK_API: true,//測試前端用true後端用false
+  API_TIMEOUT_MS: 30000,
+  //使用者訂餐API
+  USER_API_URL: "https://script.google.com/macros/s/AKfycbycUcCxk0nPzR8iS5_J9jwttu1Dvftwg9kJFJtnd_0abTksYgvCMabvl0gvis5ov78/exec",
+  //管理者API
+  ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbxL9mNcNTEyFRfnQ6VnCyMpiuTaJB1uKugpmiE-rifRsw444AHix27pMGzbNQJ8WgA/exec"
+};
